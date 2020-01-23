81331afb3cad2def0fe11933994dd21682e83d40
Brent Amrhein <brent.amrhein@turner.com>
2020-01-22 22:41:59 -0500

    Lots of changes. Trying out Mongoose.

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const path = require('path');
 const bodyParser = require('body-parser');
 // const createError = require('http-errors');
 const logger = require('morgan');
-// const monk = require('monk');
 const app = express();
+
 
 // The 'static' directory is for client-side content/styling/scripts.
 app.use(express.static('static'));
@@ -16,15 +16,11 @@ app.use('/js', express.static(path.join(__dirname, 'static/js')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
+
 // View engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Make DB accessible to routes
-// app.use((req, res, next) => {
-//   req.db = db;
-//   next();
-// });
 
 // Routes
 const indexRouter = require('./routes/index');
@@ -40,12 +36,8 @@ app.use('/logistics', logisticsRouter);
 //   next(createError(404));
 // });
 
+
 // App server
 app.listen(3010, '127.0.0.1', () => {
   console.log('brentandkatie.wedding listening on port 3010');
 });
-
-
-/**********************************************************************
- * Database functions (TODO: Move to separate file)
- *********************************************************************/
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -1,40 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const config = require('../config.json');
+const db = require('../db');
 
-router.get('/', (req, res) => {
-	// Include all the variables to make templating simple.
-	res.render('rsvp', {
-		message: '',
-		display: 'none',
-		fullname: '',
-		phone: '',
-		email: '',
-		coming: '',
-		notComing: ''
-	});
+const RSVPSchema = db.Schema({
+  fullname: { type: String, minlength: 0, maxlength: 69 },
+  phone: { type: String, minlength: 0, maxlength: 16 },
+  email: {
+    type: String,
+    minlength: 0,
+    maxlength: 69,
+    lowercase: true,
+    trim: true
+  },
+  coming: Boolean
 });
 
-router.post('/rsvp', (req, res) => {
-  const data = req.body;
+router.get('/', (req, res) => {
+    // Include all the variables to make templating simple.
+  res.render('rsvp', {
+    message: '',
+    display: 'none',
+    fullname: '',
+    phone: '',
+    email: '',
+    coming: '',
+    notComing: ''
+  });
+});
+
+router.post('/', (req, res) => {
+  // await insertThing(req.body);
+  // console.log(req.body);
+
   let message = '';
 
-  // await insertPerson(data);
-  console.log(req.body);
-
   if (req.body.coming == 'Yes') {
-  	message = "WOOO! We can't wait to see you in Moab!";
+    message = "WOOO! We can't wait to see you in Moab!";
   } else if (req.body.coming == 'No') {
-  	message = "Sorry you can't make it. We still love you... :-/";
+    message = "Sorry you can't make it. We still love you... :-/";
   }
 
   res.render('rsvp', {
-  	message: message,
-  	display: 'block',
-		fullname: req.body.fullname || '',
-		phone: req.body.phone || '',
-		email: req.body.email || '',
-		coming: req.body.coming == 'Yes' ? 'checked' : '',
-		notComing: req.body.coming == 'No' ? 'checked' : ''
+    message: message,
+    display: 'block',
+    fullname: req.body.fullname || '',
+    phone: req.body.phone || '',
+    email: req.body.email || '',
+    coming: req.body.coming == 'Yes' ? 'checked' : '',
+    notComing: req.body.coming == 'No' ? 'checked' : ''
   });
 });
 
