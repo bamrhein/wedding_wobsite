d3e029ec03bb97bd585146487aa7ab4763fa63d5
Brent Amrhein <brent.amrhein@turner.com>
2020-01-23 20:15:57 -0500

    Database backend is functional. Also some styling changes. Also confetti. :V

---
+++ db.js
@@ -0,0 +1,29 @@
+const config = require('./config.json');
+const mongoose = require('mongoose');
+
+// TODO: Add detection for dev vs prod environments
+mongoose.connect(config.dev.db.uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+// Log connection errors
+mongoose.connection.on('error',
+  console.error.bind(console, 'MongoDB connection error:'));
+
+const RSVPSchema = mongoose.Schema({
+  fullname: { type: String, minlength: 5, maxlength: 30 },
+  phone: { type: String, minlength: 10, maxlength: 16 },
+  email: {
+    type: String,
+    minlength: 3,
+    maxlength: 50,
+    lowercase: true,
+    trim: true
+  },
+  attending: Boolean
+});
+
+const RSVPModel = mongoose.model('rsvp', RSVPSchema);
+
+module.exports = { mongoose, RSVPModel };
